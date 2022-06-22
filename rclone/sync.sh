3309cf9d0f865b448a66bd607425cabac14cd14a
Kevin Haffner <khaffner@gmail.com>
2022-06-22 14:44:05 +0200

    cleanup of old logs before sync

--- rclone/sync.sh
+++ rclone/sync.sh
@@ -1,3 +1,8 @@
+# Sync is one way, so deletions must happen on source(pi). Hence the cleanup below before the sync
+
+echo "Deleting log files older than 90 days"
+find /logs/ -type f -mtime +90 -print | xargs rm -rf # https://stackoverflow.com/a/26870816
+
 while true; do
     if ping -c 4 8.8.8.8 &>/dev/null; then
         # Only attempt sync if online
