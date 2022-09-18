07c8be4d043b615b0e71ba3235178df598870f74
Kevin Haffner <khaffner@gmail.com>
2022-09-18 17:33:44 +0200

    forgot this

--- obd/logger.ps1
+++ obd/logger.ps1
@@ -9,7 +9,7 @@ while ($true) {
     # Only run if OBD is connected
     if (Test-Path -Path "/dev/obd") {
         # Tell HA OBD is online
-        Invoke-RestMethod -Uri "http://homeassistant.local:8123/api/states/sensor.OBD" -Method Post -Body @{"state" = "Connected" } -Headers $headers
+        Invoke-RestMethod -Uri "http://localhost:8123/api/states/sensor.OBD" -Method Post -Body @{"state" = "Connected" } -Headers $headers
 
         # Actually get the data from the car, store it as local csv file. https://github.com/khaffner/triplet-bmu
         python3 csvdump.py
